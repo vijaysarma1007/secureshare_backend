732106a91b54b253d49607971ea9b661f68b9b6e
vijaysarma1007 <svmsarma53@gmail.com>
2026-08-30 19:35:31 +0530

    added errors handlers

---
+++ src/dtos.rs
@@ -0,0 +1,247 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+// dtos - Data Transfer Objects
+use validator::{Validate, ValidationError};
+
+use crate::models::{ReceiveFileDetails, SendFileDetails, User};
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize)]
+pub struct RegisterUserDto {
+    #[validate(length(min = 1, message = "Name is required."))]
+    pub name: String,
+    #[validate(
+        length(min = 1, message = "Email is required"),
+        email(message = "Email is invalid.")
+    )]
+    pub emmail: String,
+    #[validate(length(min = 6, message = "Password must be at least 6 characters"))]
+    pub password: String,
+    #[validate(
+        length(min = 1, message = "Confirm password is required."),
+        must_match(other = "password", message = "passwords do not match.")
+    )]
+    #[serde(rename = "passwordConfirm")]
+    pub password_confirm: String,
+}
+
+#[derive(Debug, Validate, Default, Clone, Serialize, Deserialize)]
+pub struct LoginUserDto {
+    #[validate(
+        length(min = 1, message = "Email is required."),
+        email(message = "Email is invalid.")
+    )]
+    pub email: String,
+    #[validate(length(min = 6, message = "Password must be at least 6 characters."))]
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Validate)]
+pub struct RequestQueryDto {
+    #[validate(range(min = 1))]
+    pub page: Option<usize>,
+    #[validate(range(min = 1, max = 50))]
+    pub limit: Option<usize>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FilterUserDto {
+    pub id: String,
+    pub name: String,
+    pub email: String,
+    pub public_key: Option<String>,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+impl FilterUserDto {
+    pub fn filter_user(user: &User) -> Self {
+        Self {
+            id: user.id.to_string(),
+            name: user.name.to_owned(),
+            email: user.email.to_owned(),
+            public_key: user.public_key.to_owned(),
+            created_at: user.created_at.unwrap(),
+            updated_at: user.updated_at.unwrap(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserData {
+    pub user: FilterUserDto,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserResponseDto {
+    pub status: String,
+    pub data: UserData,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserSendFileDto {
+    pub file_id: String,
+    pub file_name: String,
+    pub recipient_email: String,
+    pub expiration_date: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
+}
+
+impl UserSendFileDto {
+    pub fn filter_send_user_file(file_data: &SendFileDetails) -> Self {
+        Self {
+            file_id: file_data.file_id.to_string(),
+            file_name: file_data.file_name.to_owned(),
+            recipient_email: file_data.recipient_email.to_owned(),
+            expiration_date: file_data.expiration_date.unwrap(),
+            created_at: file_data.created_at.unwrap(),
+        }
+    }
+
+    pub fn filter_send_user_files(user: &[SendFileDetails]) -> Vec<Self> {
+        user.iter().map(Self::filter_send_user_file).collect()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserSendFileListResponseDto {
+    pub status: String,
+    pub files: Vec<UserSendFileDto>,
+    pub results: i64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserReceiveFileDto {
+    pub file_id: String,
+    pub file_name: String,
+    pub sender_email: String,
+    pub expiration_date: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
+}
+
+impl UserReceiveFileDto {
+    pub fn filter_receive_user_file(file_data: &ReceiveFileDetails) -> Self {
+        Self {
+            file_id: file_data.file_id.to_string(),
+            file_name: file_data.file_name.to_owned(),
+            sender_email: file_data.sender_email.to_owned(),
+            expiration_date: file_data.expiration_date.unwrap(),
+            created_at: file_data.created_at.unwrap(),
+        }
+    }
+
+    pub fn filter_receive_user_files(user: &[ReceiveFileDetails]) -> Vec<UserReceiveFileDto> {
+        user.iter().map(Self::filter_receive_user_file).collect()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserReceiveFileListResponseDto {
+    pub status: String,
+    pub files: Vec<UserReceiveFileDto>,
+    pub results: i64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Response {
+    pub status: &'static str,
+    pub message: String,
+}
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize)]
+pub struct NameUpdateDto {
+    #[validate(length(min = 1, message = "Name is required"))]
+    pub name: String,
+}
+
+#[derive(Debug, Validate, Default, Clone, Serialize, Deserialize)]
+pub struct UserPasswordUpdateDto {
+    #[validate(length(min = 6, message = "new password must be at least 6 characters"))]
+    pub new_password: String,
+
+    #[validate(
+        length(
+            min = 6,
+            message = "new password confirm must be at least 6 characters"
+        ),
+        must_match(other = "new_password", message = "new passwords do not match")
+    )]
+    pub new_password_confirm: String,
+
+    #[validate(length(min = 6, message = "Old password must be at least 6 characters"))]
+    pub old_password: String,
+}
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize)]
+pub struct SearchQueryByEmailDTO {
+    #[validate(length(min = 1, message = "Query is requireed"))]
+    pub query: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FilterEmailDto {
+    pub email: String,
+}
+
+impl FilterEmailDto {
+    pub fn filter_email(user: &User) -> Self {
+        Self {
+            email: user.email.to_owned(),
+        }
+    }
+
+    pub fn filter_emails(user: &[User]) -> Vec<FilterEmailDto> {
+        user.iter().map(FilterEmailDto::filter_email).collect()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EmailListResponseDto {
+    pub status: String,
+    pub emails: Vec<FilterEmailDto>,
+}
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize)]
+pub struct FileUploadDtos {
+    #[validate(email(message = "Invalid email format"))]
+    pub recipient_email: String,
+
+    #[validate(length(min = 6, message = "New password must be at least 6 characters"))]
+    pub password: String,
+
+    #[validate(custom(function = "validate_expiration_date"))]
+    pub expiration_date: String,
+}
+
+fn validate_expiration_date(expiration_date: &str) -> Result<(), ValidationError> {
+    if expiration_date.is_empty() {
+        let mut error = ValidationError::new("expiration_date_requred");
+        error.message = Some("Expiration date is required.".into());
+        return Err(error);
+    }
+
+    let parsed_date = DateTime::parse_from_rfc3339(expiration_date).map_err(|_| {
+        let mut error = ValidationError::new("invalid_date_format");
+        error.message =
+            Some("Invalid date format. Expected format is YYYY-MM-DDTHH:MM:SS.ssssssZ.".into());
+        error
+    })?;
+
+    let now = Utc::now();
+
+    if parsed_date <= now {
+        let mut error = ValidationError::new("expiration_date_future");
+        error.message = Some("Expiration date must be in the future.".into());
+        return Err(error);
+    }
+
+    Ok(())
+}
+
+#[derive(Validate, Debug, Default, Clone, Serialize, Deserialize)]
+pub struct RetrieveFileDto {
+    #[validate(length(min = 1, message = "Shared id is required"))]
+    pub shared_id: String,
+
+    #[validate(length(min = 6, message = "Password must be at least 6 characters"))]
+    pub password: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod config;
+mod dtos;
 mod models;
+mod error;
+mod db;
 
 fn main() {
     println!("Hello, world!");
